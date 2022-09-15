ce4cf50157f474e5025e07d497830d70b10e078f
Andrew Mathieu <jesztarmgmt@gmail.com>
2022-09-15 22:19:09 +0200

    Bonjour, ça va ?

--- send.sh
+++ send.sh
@@ -4,7 +4,7 @@ echo $RANDOM >modifier.txt
 git add modifier.txt
 echo "\nEnter your message:"
 read message
-git commit -am $message
+git commit -am "$message"
 git push --force
 clear
 echo "Sent!"
